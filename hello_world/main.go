7ccad0dc79c5f2f5cbb0ad6d72294e4de50026a1
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-19 21:19:50 +0900

    chore

--- hello_world/main.go
+++ hello_world/main.go
@@ -18,11 +18,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 200, 50
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(300, 100)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
